288fde88f34afd4d49a1317589d406c3aa8be81d
Saireddy-43 <skanduku4@gitam.in>
2024-12-06 11:23:38 +0530

    intial varsion

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main() {
+    double a, b, c, x, y;
+    printf("Enter the value of a: ");
+    scanf("%lf", &a);
+    printf("Enter the value of b: ");
+    scanf("%lf", &b);
+    printf("Enter the value of c: ");
+    scanf("%lf", &c);
+    printf("Enter the value of x: ");
+    scanf("%lf", &x);
+
+    printf("The predicted value of y is: %.2lf\n", y);
+
+    return 0;
+}
